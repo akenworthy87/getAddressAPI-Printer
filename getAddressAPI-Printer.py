134a97c9dc0b909b5bc35903e39d69e757042fce
Andrew Kenworthy <akenworthy87@gmail.com>
2021-06-30 20:39:45 +0100

    Added caching functionality

--- getAddressAPI-Printer.py
+++ getAddressAPI-Printer.py
@@ -1,8 +1,9 @@
 import os
 from pymongo import MongoClient
 import requests
-# from datetime import datetime
+from datetime import datetime
 
+# These Envars are crucical to function, so throws error if not found
 try:
     MONGO_URI = os.environ.get('MONGODB_URI')
     API_KEY = os.environ['APIKEY']
@@ -20,8 +21,10 @@ def lookup_postcode_in_db(postcode):
     record = db.find_one({"postcode": postcode})
     # Check if record found
     if record is None:
-        # If not found query api
+        # If not found, query api
         record = query_api_for_addresses(postcode)
+    else:
+        print("Retrieved cached record\n")
     # Second check to see if API lookup successful
     if not record:
         print("Postcode not found in DB or API")
@@ -50,15 +53,20 @@ def query_api_for_addresses(postcode):
     status_code = response.status_code
     response = response.json()
     if status_code == 200:
-        # cache_response_to_db(response)
+        cache_response_to_db(response, postcode)
         return response
     # Displays error if valid response not received
     else:
         print(f"Response Error {status_code}: {response['Message']}")
         return None
 
-# Store record in DB
-# Add postcode and timestamp to record
+
+def cache_response_to_db(response, postcode):
+    # Add postcode and timestamp to record
+    response["postcode"] = postcode
+    response["cached_date"] = datetime.utcnow()
+    # Store record in DB
+    db.insert_one(response)
 
 
 # 3. Print_Addresses() takes a dict of format: {addresses:[*address*:[*lines*]]}
@@ -73,7 +81,7 @@ def print_addresses(record):
         print()
 
 
-# 1. Console Input: Ask user to provide Postcode to look up, or (q)uit
+# Loops around and asks user to provide Postcode to look up, or (q)uit
 if __name__ == '__main__':
     while True:
         # print()
