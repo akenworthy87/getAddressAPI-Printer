8d3f1ef1ba5d27115d7ddc479e59262575dbeecf
Andrew Kenworthy <akenworthy87@gmail.com>
2021-06-30 19:26:25 +0100

    Added error handling if EnVars not set Wrote API request functionality

--- getAddressAPI-Printer.py
+++ getAddressAPI-Printer.py
@@ -1,18 +1,17 @@
 import os
 from pymongo import MongoClient
+import requests
 # from datetime import datetime
 
-MONGO_URI = os.environ.get('MONGODB_URI')
-API_KEY = os.environ.get('API_KEY')
+try:
+    MONGO_URI = os.environ.get('MONGODB_URI')
+    API_KEY = os.environ['APIKEY']
+except KeyError as e:
+    print(f"ERROR: Environment variable: {e} is not set")
+    quit()
 
 client = MongoClient(MONGO_URI)
 db = client.getAddress.postcodes
-
-
-# print(
-#     db.find_one(
-#         {"postcode": "NN1 3ER"}
-#     ))
 
 
 # 2. Request Postcode record from DB:
@@ -26,21 +25,46 @@ def lookup_postcode_in_db(postcode):
     # Second check to see if API lookup successful
     if not record:
         print("Postcode not found in DB or API")
+        return
     else:
         # Print addresses if record found
         print_addresses(record)
 
 
 # Query getAddress API for current record (format & sorted = true)
-# Store record in DB
-# Add postcode and timestamp to record
-# Send record to Print_Addresses()
 def query_api_for_addresses(postcode):
     print(f"Calling API for {postcode}")
+
+    endpoint = f"https://api.getAddress.io/find/{postcode}"
+    query_params = {
+        "api-key": API_KEY,
+        "format": "true",
+        "sorted": "true",
+    }
+    try:
+        response = requests.get(endpoint, params=query_params)
+    except requests.ConnectionError:
+        print(f"Unable to connect to: '{endpoint}'")
+        return None
+
+    status_code = response.status_code
+    response = response.json()
+    if status_code == 200:
+        # cache_response_to_db(response)
+        return response
+    # Displays error if valid response not received
+    else:
+        print(f"Response Error {status_code}: {response['Message']}")
+        return None
+
+# Store record in DB
+# Add postcode and timestamp to record
 
 
 # 3. Print_Addresses() takes a dict of format: {addresses:[*address*:[*lines*]]}
 def print_addresses(record):
+    if not record["addresses"]:
+        print("No addresses in record")
     for address in record["addresses"]:
         for line in address:
             # print(line) if not blank
@@ -55,7 +79,7 @@ if __name__ == '__main__':
         # print()
         postcode = input("Enter postcode to lookup, or (q)uit: ")
         # Checking if user wants to quit
-        if postcode == "q":
+        if postcode.lower() in ["q", "quit"]:
             quit()
         # Checking if postcode isn't blank, loops back if is
         if postcode:
